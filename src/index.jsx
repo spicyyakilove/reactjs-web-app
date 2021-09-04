8489a6f2d60f483ea605a54c84451aadcc1e1d68
Gareth Dawson <gwdawson.work@gmail.com>
2021-09-04 20:26:07 +0100

    Updated TODO

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 /*
 TODO:
+Add css root variables to index.css and import into each page
 Add Login
 Add styling for Homepage, Login, Signup (Responsive)
 Add react-router-dom (Pages)
