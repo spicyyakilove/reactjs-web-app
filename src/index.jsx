6234d6b7a8e61562a782e9b1ad62d2a7c62e58b7
Gareth Dawson <gwdawson.work@gmail.com>
2021-09-04 20:23:26 +0100

    Added TODO

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+/*
+TODO:
+Add Login
+Add styling for Homepage, Login, Signup (Responsive)
+Add react-router-dom (Pages)
+*/
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
