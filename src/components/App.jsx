efaf2167bf05d991cffb9a0d3b6fe85f83d82bb0
Gareth Dawson <gwdawson.work@gmail.com>
2021-09-04 22:26:15 +0100

    Started using react-router-dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import './App.css';
+import Navbar from './Navbar';
 import Homepage from './Homepage';
 import Signup from './Signup';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div className='App'>
-            <Homepage />
-            <p> </p>
-            <Signup />
+            <Router>
+                <Navbar />
+                <Route path='/' component={Homepage} />
+                <Route path='/signup' component={Signup} /> {/* ERROR: going to '/signup' will not load the page. */}
+            </Router>
         </div>
     );
 }
