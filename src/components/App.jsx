8efefc09db7ac5bd4acc59437ba548a5eb9d6255
Gareth Dawson <gwdawson.work@gmail.com>
2021-09-05 13:23:14 +0100

    Trying to get react-router working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,41 @@
-import './App.css';
-import Navbar from './Navbar';
-import Homepage from './Homepage';
-import Signup from './Signup';
+// import './App.css';
+// import Navbar from './Navbar';
+// import Homepage from './Homepage';
+// import Signup from './Signup';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// function App() {
+//     return (
+//         <Router>
+//             <div className='App'>
+//                 <Switch>
+//                     <Navbar />
+//                     <Route exact path='/'>
+//                         <Homepage />
+//                     </Route>
+//                     <Route path='/signup'>
+//                         <Signup />
+//                     </Route>
+//                 </Switch>
+//             </div>
+//         </Router>
+//     );
+// }
+
+// export default App;
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Homepage = () => <div>homepage</div>;
+const Signup = () => <div>signup</div>;
 
 function App() {
     return (
         <Router>
             <div className='App'>
-                <Switch>
-                    <Navbar />
-                    <Route exact path='/'>
-                        <Homepage />
-                    </Route>
-                    <Route path='/signup'>
-                        <Signup />
-                    </Route>
-                </Switch>
+                <h1>NAV</h1>
+                <Route path='/homepage' component={Homepage} />
+                <Route path='/signup' component={Signup} />
             </div>
         </Router>
     );
