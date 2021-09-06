25da0101acab9cc41a59e6f588e0385cec8c1448
Gareth Dawson <gwdawson.work@gmail.com>
2021-09-06 22:35:14 +0100

    Removed nav buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,35 +12,13 @@ export default function Navbar() {
                         <p>PROJECT NAME</p>
                     </Link>
                 </div>
-                <div className='NavLinks'>
-                    <Link to='#'>
-                        <Button className='NavLink btn-secondary btn-sm' style={{ opacity: '100% !important' }}>
-                            Navbar Item 1
-                        </Button>
-                    </Link>
-                    <Link to='#'>
-                        <Button className='NavLink btn-secondary btn-sm' style={{ opacity: '100% !important' }}>
-                            Navbar Item 2
-                        </Button>
-                    </Link>
-                    <Link to='#'>
-                        <Button className='NavLink btn-secondary btn-sm' style={{ opacity: '100% !important' }}>
-                            Navbar Item 3
-                        </Button>
-                    </Link>
-                    <Link to='#'>
-                        <Button className='NavLink LastNavLink btn-secondary btn-sm' style={{ opacity: '100% !important' }}>
-                            Navbar Item 4
-                        </Button>
-                    </Link>
-                </div>
                 <div className='Authentication'>
-                    <Link to='#'>
+                    <Link to='/reactjs-web-app/auth/login'>
                         <Button className='btn btn-secondary btn-sm' style={{ borderRadius: '100px' }}>
                             Log in
                         </Button>
                     </Link>
-                    <Link to='/reactjs-web-app/signup'>
+                    <Link to='/reactjs-web-app/auth/signup'>
                         <Button
                             className='btn-color btn btn-primary btn-sm'
                             style={{
